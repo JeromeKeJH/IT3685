57e340861b52f56a72b8ffff5be2fd05e9b5da3e
Interns <interns@sice.sbmedia.sg>
2022-01-22 20:05:54 +0800

    Product Module

--- Product.aspx.cs
+++ Product.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Configuration;
 
 namespace IT3685
 {
@@ -11,7 +14,17 @@ namespace IT3685
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["IT3685"].ConnectionString);
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM product", con);
+            con.Open();
+            
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            adapter.SelectCommand = cmd;
 
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet);
+            productsRepeater.DataSource = dataSet;
+            productsRepeater.DataBind();
         }
     }
 }
